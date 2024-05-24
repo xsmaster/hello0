76d6a092e4b538a7e37b0189a47ce71f4cd3838e
xsmaster <165388332+xsmaster@users.noreply.github.com>
2024-05-24 15:44:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let A = +prompt(" 1 Birinchi sonni tanlang");
+let B = +prompt(" 2 Ikkinchi sonni tanlang");
+let Amall = +prompt("Amalllarni tanlang \n 1) qoshish \n 2) ayirish \n 3) kopaytirish \n 4) bolish")
+if(Amall == 1){
+    alert( A + B )
+}
+if(Amall == 2){
+    alert( A - B )
+}
+if(Amall == 3){
+    alert( A * B )
+}
+if(Amall == 4){
+    alert( A / B )
+}
